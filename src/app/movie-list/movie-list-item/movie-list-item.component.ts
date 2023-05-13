9d1b5d705b9e24b09644a67ec725886692cdd699
davidIvankov <david.ivankov96@gmail.com>
2023-05-13 16:21:13 +0200

    initial commit

---
+++ src/app/movie-list/movie-list-item/movie-list-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-list-item',
+  templateUrl: './movie-list-item.component.html',
+  styleUrls: ['./movie-list-item.component.css']
+})
+export class MovieListItemComponent {
+
+}
