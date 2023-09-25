ef7565038bf6a8b9f4cad0fed32dd88ceee3d240
davidIvankov <david.ivankov96@gmail.com>
2023-09-25 07:35:10 +0200

    added disconnect from the mutationObserver and unsubscription using pipe method take for getMovies

--- src/app/components/movies/movie-list/movie-list.component.ts
+++ src/app/components/movies/movie-list/movie-list.component.ts
@@ -6,38 +6,45 @@ import {
   ElementRef,
   AfterViewInit,
   Output,
+  OnDestroy,
 } from '@angular/core';
-import { MoviesService } from '../movies-service.service';
+import { MoviesService } from '../movies.service';
 import { Movie } from './movie.model';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
   styleUrls: ['./movie-list.component.css'],
 })
-export class MovieListComponent implements OnInit, AfterViewInit {
+export class MovieListComponent implements OnInit, AfterViewInit, OnDestroy {
   @Output() movies: Movie[] = [];
   page = 1;
-  private changes: MutationObserver;
-  @ViewChild('left')
-  myReference: ElementRef;
+  @ViewChild('left') myReference: ElementRef;
   height: number;
+  private changes: MutationObserver;
 
   constructor(private movieService: MoviesService) {}
 
   ngOnInit() {
+    this.getMovies();
+  }
+
+  private getMovies() {
     this.movieService
       .getMovies(this.page)
+      .pipe(take(1))
       .subscribe((res) => (this.movies = res));
   }
 
   ngAfterViewInit(): void {
+    const tracked = document.querySelector('.tracked');
     this.changes = new MutationObserver(() => {
       if (this.myReference) {
         this.height = this.myReference.nativeElement.offsetHeight;
       }
     });
-    this.changes.observe(document.querySelector('.tracked'), {
+    this.changes.observe(tracked, {
       attributes: true,
       childList: true,
       characterData: true,
@@ -48,9 +55,11 @@ export class MovieListComponent implements OnInit, AfterViewInit {
   onScroll(): void {
     if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
       this.page++;
-      this.movieService
-        .getMovies(this.page)
-        .subscribe((res) => (this.movies = res));
+      this.getMovies();
     }
+  }
+
+  ngOnDestroy(): void {
+    this.changes.disconnect();
   }
 }
